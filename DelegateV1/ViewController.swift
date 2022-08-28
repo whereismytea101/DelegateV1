a69794b74c5d0b7d312dc68ed75a37f386e2c9c9
Samir <imnotajedi@icloud.com>
2022-08-28 23:32:51 +0300

    Initial Commit

---
+++ DelegateV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateV1
+//
+//  Created by Самир Кафаров on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
